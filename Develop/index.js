0feac0a0361068b6f5f4e71b8b49002aab2d2df9
Alex Tran <Lockey5891@aol.com>
2024-05-24 11:42:40 -0700

    added code to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,66 @@
 // TODO: Include packages needed for this application
+// inquier packages 8.2.4
+const inquier = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your README?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description to your README?',
+      },
+      {
+        type: 'input',
+        name: 'table of contents',
+        message: 'What is your table of contents?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are some installtion that you use?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the usage for your README?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What are the License you use for your README?',
+        choices:[example,example2]
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What or Who contributed to you project?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the tests you use?',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'Do you have any questions?',
+      },
+    ])
+    .then((answers) => {
+      const READMEPageContent = generateREADME(answers);
+  
+      fs.writeFile('README.md', htmlPageContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md!')
+      );
+      
+   
+});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +69,13 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+const init = () => {
+    promptUser()
+      // Use writeFile method imported from fs.promises to use promises instead of
+      // a callback function
+      .then((answers) => writeFile('index.html', generateHTML(answers)))
+      .then(() => console.log('Successfully wrote to index.html'))
+      .catch((err) => console.error(err));
+  };
+  
+init()};
