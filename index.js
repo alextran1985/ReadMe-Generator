0ea8300f2e8ddc66034a272d4f8838b9b6d6f753
Alex Tran <Lockey5891@aol.com>
2024-05-24 13:26:09 -0700

    Testing the generator for readme

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 // inquier packages 8.2.4
-const inquier = require('inquirer');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = () => {
@@ -34,7 +36,7 @@ const questions = () => {
         type: 'list',
         name: 'license',
         message: 'What are the License you use for your README?',
-        choices:[example,example2]
+        choices:['']
       },
       {
         type: 'input',
@@ -50,32 +52,51 @@ const questions = () => {
         type: 'input',
         name: 'questions',
         message: 'Do you have any questions?',
-      },
+      }, 
     ])
     .then((answers) => {
-      const READMEPageContent = generateREADME(answers);
+        console.log("Data Captured: ", answers);
+
+      const READMEPageContent = generateMarkdown(answers);
+      console.log("Generated Dataset: ", READMEPageContent);
+      console.log("Data type: ", typeof READMEPageContent);  // string 
   
-      fs.writeFile('README.md', htmlPageContent, (err) =>
+      fs.writeFile('README.md', READMEPageContent, (err) =>
         err ? console.log(err) : console.log('Successfully created README.md!')
       );
       
    
-});
+    });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-const init = () => {
-    promptUser()
+function init() {
+   questions();
+   
+   /* promptUser()
       // Use writeFile method imported from fs.promises to use promises instead of
       // a callback function
-      .then((answers) => writeFile('index.html', generateHTML(answers)))
-      .then(() => console.log('Successfully wrote to index.html'))
+      .then((answers) => writeFile('index.html', generateREADME(answers)))
+      .then(() => console.log('Successfully wrote to README.md'))
       .catch((err) => console.error(err));
-  };
+      */ 
+}
+
+// Function call to initialize app
+//const init = () => {};
   
-init()};
+init();
+
+
+// --- What Order should things(EVENTS) happen in our Program? -- // 
+
+// 1) Capture User data --> use inquirer.prompt() --> questions() | handle a async PROMISE
+
+// Once we HAVE DATA --> ?
+//     we can store that data in a variable --> .then(answers)
+// 2) What do we DO with the DATA --> we want to CREATE a README document (string object)
+
+// 3) Lastly we SAVE --> WRITE that DATA to a file 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,32 @@
-# Readme generator<Your-Project-Title>
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if(!license) {
+    return '';
+  }
+  return "## Liscense"
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}<Your-Project-Title>
 
   
   ## Description
-  This is my description
+  ${data.description}
 
 
   
   ## Table of Contents (Optional)
-  undefined
+  ${data.tableofcontents}
   
   - [Installation](#installation)
   - [Usage](#usage)
@@ -15,18 +34,18 @@
   - [License](#license)
   
   ## Installation
-  none
+  ${data.installation}
   What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
   
   ## Usage
-  something
+  ${data.usage}
   Provide instructions and examples for use. Include screenshots as needed.
   
   
   ![alt text](assets/images/screenshot.png)
   
   ## Credits
-  undefined
+  ${data.credits}
   List your collaborators, if any, with links to their GitHub profiles.
   
   If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
@@ -34,7 +53,7 @@
   If you followed tutorials, include links to those here as well.
   
   ## License
-  
+  ${data.license}
   The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
   
   ---
@@ -42,24 +61,29 @@
   🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
   
   ## Badges
-  undefined
+  ${data.badges}
   ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
   
   Badges aren't necessary, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
   
   ## Questions
-  no
+  ${data.questions}
   
   If your project has a lot of features, list them here.
   
   ## How to Contribute
-  Alex Tran
+  ${data.contributing}
   
   If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
   
   ## Tests
-  yes
+  ${data.tests}
   
   Go the extra mile and write tests for your application. Then provide examples on how to run them here.
   
 
+`;
+}
+
+
+module.exports = generateMarkdown;
